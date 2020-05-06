757ac6d0125e392c866e90b919893ef6023ef274
montanachen <montanachen1112@gmail.com>
2020-05-06 14:30:39 +0800

    add task.c to msys2

--- code/c/06-os1windows/02-mingw/01-cat/cat.c
+++ code/c/06-os1windows/02-mingw/01-cat/cat.c
@@ -7,8 +7,8 @@ char text[TEXT_SIZE];
 
 int main(int argc, char *argv[]) {
   char *fileName = argv[1];
-  int fd = open(fileName, O_RDONLY);
-  int len = read(fd, text, TEXT_SIZE-1);
+  int fd = open(fileName, O_RDONLY);     // FILE *file = fopen(fileName, "rt");
+  int len = read(fd, text, TEXT_SIZE-1); // int len = fread(text, 1, TEXT_SIZE-1, file);
   text[len] = 0;
   printf("%s\n", text);
 }
