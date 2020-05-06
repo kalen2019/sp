ade1bac02d5366769784d6eebe12520ff6240681
montanachen <montanachen1112@gmail.com>
2020-05-06 16:20:33 +0800

    move georgeMary.c

--- code/c/06-os1windows/04-pthread/norace.s
+++ code/c/06-os1windows/04-pthread/norace.s
@@ -1,149 +1,125 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 13
-	.globl	_inc
-	.p2align	4, 0x90
-_inc:                                   ## @inc
-	.cfi_startproc
-## BB#0:
+	.file	"norace.c"
+	.text
+	.globl	mutex1
+	.data
+	.align 8
+mutex1:
+	.quad	19
+	.globl	counter
+	.bss
+	.align 4
+counter:
+	.space 4
+	.text
+	.globl	inc
+	.def	inc;	.scl	2;	.type	32;	.endef
+	.seh_proc	inc
+inc:
 	pushq	%rbp
-Lcfi0:
-	.cfi_def_cfa_offset 16
-Lcfi1:
-	.cfi_offset %rbp, -16
+	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
-Lcfi2:
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
-LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
-	cmpl	$100000, -4(%rbp)       ## imm = 0x186A0
-	jge	LBB0_4
-## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
-	leaq	_mutex1(%rip), %rdi
-	callq	_pthread_mutex_lock
-	leaq	_mutex1(%rip), %rdi
-	movl	_counter(%rip), %ecx
-	addl	$1, %ecx
-	movl	%ecx, _counter(%rip)
-	movl	%eax, -8(%rbp)          ## 4-byte Spill
-	callq	_pthread_mutex_unlock
-	movl	%eax, -12(%rbp)         ## 4-byte Spill
-## BB#3:                                ##   in Loop: Header=BB0_1 Depth=1
-	movl	-4(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -4(%rbp)
-	jmp	LBB0_1
-LBB0_4:
-	xorl	%eax, %eax
-                                        ## kill: %RAX<def> %EAX<kill>
-	addq	$16, %rsp
-	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.globl	_dec
-	.p2align	4, 0x90
-_dec:                                   ## @dec
-	.cfi_startproc
-## BB#0:
-	pushq	%rbp
-Lcfi3:
-	.cfi_def_cfa_offset 16
-Lcfi4:
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-Lcfi5:
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
-LBB1_1:                                 ## =>This Inner Loop Header: Depth=1
-	cmpl	$100000, -4(%rbp)       ## imm = 0x186A0
-	jge	LBB1_4
-## BB#2:                                ##   in Loop: Header=BB1_1 Depth=1
-	leaq	_mutex1(%rip), %rdi
-	callq	_pthread_mutex_lock
-	leaq	_mutex1(%rip), %rdi
-	movl	_counter(%rip), %ecx
-	subl	$1, %ecx
-	movl	%ecx, _counter(%rip)
-	movl	%eax, -8(%rbp)          ## 4-byte Spill
-	callq	_pthread_mutex_unlock
-	movl	%eax, -12(%rbp)         ## 4-byte Spill
-## BB#3:                                ##   in Loop: Header=BB1_1 Depth=1
-	movl	-4(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -4(%rbp)
-	jmp	LBB1_1
-LBB1_4:
-	xorl	%eax, %eax
-                                        ## kill: %RAX<def> %EAX<kill>
-	addq	$16, %rsp
-	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.globl	_main
-	.p2align	4, 0x90
-_main:                                  ## @main
-	.cfi_startproc
-## BB#0:
-	pushq	%rbp
-Lcfi6:
-	.cfi_def_cfa_offset 16
-Lcfi7:
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-Lcfi8:
-	.cfi_def_cfa_register %rbp
+	.seh_setframe	%rbp, 0
 	subq	$48, %rsp
-	leaq	-8(%rbp), %rdi
-	xorl	%eax, %eax
-	movl	%eax, %ecx
-	leaq	_inc(%rip), %rdx
-	movq	%rcx, %rsi
-	callq	_pthread_create
-	leaq	-16(%rbp), %rdi
-	xorl	%r8d, %r8d
-	movl	%r8d, %ecx
-	leaq	_dec(%rip), %rdx
-	movq	%rcx, %rsi
-	movl	%eax, -20(%rbp)         ## 4-byte Spill
-	callq	_pthread_create
-	xorl	%r8d, %r8d
-	movl	%r8d, %esi
-	movq	-8(%rbp), %rdi
-	movl	%eax, -24(%rbp)         ## 4-byte Spill
-	callq	_pthread_join
-	xorl	%r8d, %r8d
-	movl	%r8d, %esi
-	movq	-16(%rbp), %rdi
-	movl	%eax, -28(%rbp)         ## 4-byte Spill
-	callq	_pthread_join
-	leaq	L_.str(%rip), %rdi
-	movl	_counter(%rip), %esi
-	movl	%eax, -32(%rbp)         ## 4-byte Spill
-	movb	$0, %al
-	callq	_printf
-	xorl	%esi, %esi
-	movl	%eax, -36(%rbp)         ## 4-byte Spill
-	movl	%esi, %eax
+	.seh_stackalloc	48
+	.seh_endprologue
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	leaq	mutex1(%rip), %rcx
+	call	pthread_mutex_lock
+	movl	counter(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, counter(%rip)
+	leaq	mutex1(%rip), %rcx
+	call	pthread_mutex_unlock
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$99999, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
 	addq	$48, %rsp
 	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.section	__DATA,__data
-	.globl	_mutex1                 ## @mutex1
-	.p2align	3
-_mutex1:
-	.quad	850045863               ## 0x32aaaba7
-	.space	56
-
-	.globl	_counter                ## @counter
-.zerofill __DATA,__common,_counter,4,2
-	.section	__TEXT,__cstring,cstring_literals
-L_.str:                                 ## @.str
-	.asciz	"counter=%d\n"
-
-
-.subsections_via_symbols
+	ret
+	.seh_endproc
+	.globl	dec
+	.def	dec;	.scl	2;	.type	32;	.endef
+	.seh_proc	dec
+dec:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	movl	$0, -4(%rbp)
+	jmp	.L6
+.L7:
+	leaq	mutex1(%rip), %rcx
+	call	pthread_mutex_lock
+	movl	counter(%rip), %eax
+	subl	$1, %eax
+	movl	%eax, counter(%rip)
+	leaq	mutex1(%rip), %rcx
+	call	pthread_mutex_unlock
+	addl	$1, -4(%rbp)
+.L6:
+	cmpl	$99999, -4(%rbp)
+	jle	.L7
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "counter=%d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	leaq	-8(%rbp), %rax
+	movl	$0, %r9d
+	leaq	inc(%rip), %r8
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_create
+	leaq	-16(%rbp), %rax
+	movl	$0, %r9d
+	leaq	dec(%rip), %r8
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_create
+	movq	-8(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_join
+	movq	-16(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_join
+	movl	counter(%rip), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 9.1.0"
+	.def	pthread_mutex_lock;	.scl	2;	.type	32;	.endef
+	.def	pthread_mutex_unlock;	.scl	2;	.type	32;	.endef
+	.def	pthread_create;	.scl	2;	.type	32;	.endef
+	.def	pthread_join;	.scl	2;	.type	32;	.endef
+	.def	printf;	.scl	2;	.type	32;	.endef
--- code/c/06-os1windows/04-pthread/race.s
+++ code/c/06-os1windows/04-pthread/race.s
@@ -1,126 +1,110 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 13
-	.globl	_inc
-	.p2align	4, 0x90
-_inc:                                   ## @inc
-	.cfi_startproc
-## BB#0:
+	.file	"race.c"
+	.text
+	.globl	counter
+	.bss
+	.align 4
+counter:
+	.space 4
+	.text
+	.globl	inc
+	.def	inc;	.scl	2;	.type	32;	.endef
+	.seh_proc	inc
+inc:
 	pushq	%rbp
-Lcfi0:
-	.cfi_def_cfa_offset 16
-Lcfi1:
-	.cfi_offset %rbp, -16
+	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
-Lcfi2:
-	.cfi_def_cfa_register %rbp
+	.seh_setframe	%rbp, 0
+	subq	$16, %rsp
+	.seh_stackalloc	16
+	.seh_endprologue
 	movl	$0, -4(%rbp)
-LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
-	cmpl	$100000000, -4(%rbp)    ## imm = 0x5F5E100
-	jge	LBB0_4
-## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
-	movl	_counter(%rip), %eax
+	jmp	.L2
+.L3:
+	movl	counter(%rip), %eax
 	addl	$1, %eax
-	movl	%eax, _counter(%rip)
-## BB#3:                                ##   in Loop: Header=BB0_1 Depth=1
-	movl	-4(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -4(%rbp)
-	jmp	LBB0_1
-LBB0_4:
-	xorl	%eax, %eax
-                                        ## kill: %RAX<def> %EAX<kill>
+	movl	%eax, counter(%rip)
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$99999999, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
+	addq	$16, %rsp
 	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.globl	_dec
-	.p2align	4, 0x90
-_dec:                                   ## @dec
-	.cfi_startproc
-## BB#0:
+	ret
+	.seh_endproc
+	.globl	dec
+	.def	dec;	.scl	2;	.type	32;	.endef
+	.seh_proc	dec
+dec:
 	pushq	%rbp
-Lcfi3:
-	.cfi_def_cfa_offset 16
-Lcfi4:
-	.cfi_offset %rbp, -16
+	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
-Lcfi5:
-	.cfi_def_cfa_register %rbp
+	.seh_setframe	%rbp, 0
+	subq	$16, %rsp
+	.seh_stackalloc	16
+	.seh_endprologue
 	movl	$0, -4(%rbp)
-LBB1_1:                                 ## =>This Inner Loop Header: Depth=1
-	cmpl	$100000000, -4(%rbp)    ## imm = 0x5F5E100
-	jge	LBB1_4
-## BB#2:                                ##   in Loop: Header=BB1_1 Depth=1
-	movl	_counter(%rip), %eax
+	jmp	.L6
+.L7:
+	movl	counter(%rip), %eax
 	subl	$1, %eax
-	movl	%eax, _counter(%rip)
-## BB#3:                                ##   in Loop: Header=BB1_1 Depth=1
-	movl	-4(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -4(%rbp)
-	jmp	LBB1_1
-LBB1_4:
-	xorl	%eax, %eax
-                                        ## kill: %RAX<def> %EAX<kill>
+	movl	%eax, counter(%rip)
+	addl	$1, -4(%rbp)
+.L6:
+	cmpl	$99999999, -4(%rbp)
+	jle	.L7
+	movl	$0, %eax
+	addq	$16, %rsp
 	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.globl	_main
-	.p2align	4, 0x90
-_main:                                  ## @main
-	.cfi_startproc
-## BB#0:
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "counter=%d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
 	pushq	%rbp
-Lcfi6:
-	.cfi_def_cfa_offset 16
-Lcfi7:
-	.cfi_offset %rbp, -16
+	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
-Lcfi8:
-	.cfi_def_cfa_register %rbp
+	.seh_setframe	%rbp, 0
 	subq	$48, %rsp
-	leaq	-8(%rbp), %rdi
-	xorl	%eax, %eax
-	movl	%eax, %ecx
-	leaq	_inc(%rip), %rdx
-	movq	%rcx, %rsi
-	callq	_pthread_create
-	leaq	-16(%rbp), %rdi
-	xorl	%r8d, %r8d
-	movl	%r8d, %ecx
-	leaq	_dec(%rip), %rdx
-	movq	%rcx, %rsi
-	movl	%eax, -20(%rbp)         ## 4-byte Spill
-	callq	_pthread_create
-	xorl	%r8d, %r8d
-	movl	%r8d, %esi
-	movq	-8(%rbp), %rdi
-	movl	%eax, -24(%rbp)         ## 4-byte Spill
-	callq	_pthread_join
-	xorl	%r8d, %r8d
-	movl	%r8d, %esi
-	movq	-16(%rbp), %rdi
-	movl	%eax, -28(%rbp)         ## 4-byte Spill
-	callq	_pthread_join
-	leaq	L_.str(%rip), %rdi
-	movl	_counter(%rip), %esi
-	movl	%eax, -32(%rbp)         ## 4-byte Spill
-	movb	$0, %al
-	callq	_printf
-	xorl	%esi, %esi
-	movl	%eax, -36(%rbp)         ## 4-byte Spill
-	movl	%esi, %eax
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	leaq	-8(%rbp), %rax
+	movl	$0, %r9d
+	leaq	inc(%rip), %r8
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_create
+	leaq	-16(%rbp), %rax
+	movl	$0, %r9d
+	leaq	dec(%rip), %r8
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_create
+	movq	-8(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_join
+	movq	-16(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	pthread_join
+	movl	counter(%rip), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
 	addq	$48, %rsp
 	popq	%rbp
-	retq
-	.cfi_endproc
-
-	.globl	_counter                ## @counter
-.zerofill __DATA,__common,_counter,4,2
-	.section	__TEXT,__cstring,cstring_literals
-L_.str:                                 ## @.str
-	.asciz	"counter=%d\n"
-
-
-.subsections_via_symbols
+	ret
+	.seh_endproc
+	.ident	"GCC: (GNU) 9.1.0"
+	.def	pthread_create;	.scl	2;	.type	32;	.endef
+	.def	pthread_join;	.scl	2;	.type	32;	.endef
+	.def	printf;	.scl	2;	.type	32;	.endef
